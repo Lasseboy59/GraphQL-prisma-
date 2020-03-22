8eb04ade5a94faab3686395e2486ab8a4005d220
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-22 15:10:29 +0200

    added login mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,18 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
+// const dummy = async () => {
+//   const email = 'lasse@example.com'
+//   const password = 'red12345'
+
+//   const hashedPassword = '$2a$10$Eg5IcXqAnJ.A5DdskjvJ8O0MZ4YAkbK/woZ/WJkavjkwM40EH5yGO'
+
+//   const isMatch = await bcrypt.compare(password, hashedPassword)
+//   console.log(isMatch)
+// }
+// dummy()
+
+
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
     if (args.data.password.length < 8) {
@@ -14,6 +26,28 @@ const Mutation = {
         password
       }
     })
+
+    return {
+      user,
+      token: jwt.sign({ userId: user.id }, 'thisisasecret')
+    }
+  },
+  async login(parent, args, { prisma }, info) {
+    const user = await prisma.query.user({
+      where: {
+        email: args.data.email
+      }
+    })
+
+    if (!user) {
+      throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(args.data.password, user.password)
+
+    if (!isMatch) {
+      throw new Error('Unable to login')
+    }
 
     return {
       user,
