6b9ecd8db08b72f28cf8382e4029f33137ce3487
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-23 18:27:00 +0200

    token expiration

---
+++ src/utils/generateToken.js
@@ -0,0 +1,7 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, 'thisisasecret', { expiresIn: '1 hour' })
+}
+
+export { generateToken as default }
