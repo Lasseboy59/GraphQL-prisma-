125a8d267d0aed6c499201c502a32db93a6989e9
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-24 13:04:18 +0200

    sorting data

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, 'thisisasecret', { expiresIn: '1 hour' })
+  return jwt.sign({ userId }, 'thisisasecret', { expiresIn: '1 day' })
 }
 
 export { generateToken as default }
