20c66bf3554cac38a84b949d0625607871bbf6dd
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-25 15:46:32 +0200

    seeding test database

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,19 +1,55 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost, { gql } from 'apollo-boost'
+import bcrypt from 'bcryptjs'
 import prisma from '../src/prisma'
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000'
 })
 
-test('Should create a new user', async () => {
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts()
+  await prisma.mutation.deleteManyUsers()
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: 'Timo Mäki',
+      email: 'timo@example.com',
+      password: bcrypt.hashSync('secret123')
+    }
+  })
+  await prisma.mutation.createPost({
+    data: {
+      title: 'My published post',
+      body: '',
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+  await prisma.mutation.createPost({
+    data: {
+      title: 'My draft post',
+      body: '',
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+})
 
+test('Should create a new user', async () => {
   const createUser = gql`
         mutation {
             createUser(
                 data: {
-                    name: "Timo",
-                    email: "timo@example.com",
+                    name: "Lasse",
+                    email: "lasse@example.com",
                     password: "secret123"
                 }
             ){
