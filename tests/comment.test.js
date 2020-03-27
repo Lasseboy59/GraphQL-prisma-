284b7bc3063e0ce4fc16fc76d7727d4a05c95cf3
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-27 16:27:05 +0200

    subscription tests failing

--- tests/comment.test.js
+++ tests/comment.test.js
@@ -1,8 +1,8 @@
 import 'cross-fetch/polyfill'
-import seedDatabase, { userOne, userTwo, commentTwo, commentOne } from './utils/seedDatabase'
+import seedDatabase, { userOne, commentTwo, commentOne, postOne } from './utils/seedDatabase'
 import getClient from './utils/getClient'
 import prisma from '../src/prisma'
-import { deleteComment } from './utils/operations'
+import { deleteComment, subscribeToComments, subscribeToPosts } from './utils/operations'
 
 beforeEach(seedDatabase)
 
@@ -17,7 +17,6 @@ test('Should delete own comment', async () => {
   expect(exists).toBe(false)
 })
 
-
 test('Should not delete other users comments', async () => {
   const client = getClient(userOne.jwt)
   const variables = {
@@ -28,3 +27,28 @@ test('Should not delete other users comments', async () => {
     client.mutate({ mutation: deleteComment, variables })
   ).rejects.toThrow()
 })
+
+test('Should subscribe to comments for a post', async (done) => {
+  const variables = {
+    postId: postOne.post.id
+  }
+  client.subscribe({ query: subscribeToComments, variables }).subscribe({
+    next(response) {
+      expect(response.data.comment.mutation).toBe('DELETED')
+      done()
+    }
+  })
+
+  await prisma.mutation.deleteComment({ where: { id: commentOne.comment.id } })
+})
+
+// test('Should subscribe to changes for published posts', async (done) => {
+//   client.subscribe({ query: subscribeToPosts }).subscribe({
+//       next(response) {
+//           expect(response.data.post.mutation).toBe('DELETED')
+//           done()
+//       }
+//   })
+
+//   await prisma.mutation.deletePost({ where: { id: postOne.post.id } })
+// })
